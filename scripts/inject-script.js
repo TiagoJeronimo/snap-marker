36b339b9c979d62fcbf302ceaa07b06452b03656
Tiago Jerónimo Silva <tiagosilvajeronimo@gmail.com>
2021-11-26 22:43:13 +0100

    Cleaning

--- scripts/inject-script.js
+++
@@ -1,9 +0,0 @@
-// inject-script.js
-
-const foreground_entry_point = document.createElement("div");
-let reactJS_script = document.createElement("script");
-
-foreground_entry_point.id = "root";
-
-foreground_entry_point.appendChild(reactJS_script);
-document.querySelector("body").appendChild(foreground_entry_point);
